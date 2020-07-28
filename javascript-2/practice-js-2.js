3ba0421245d4f47de65921cccc1cf2a2010c7104
Riley Hatch <riley@MacBook-Pro.local>
2020-07-28 07:48:43 -0700

    updated file structure, working on readme

--- javascript-2/practice-js-2.js
+++ javascript-2/practice-js-2.js
@@ -1,10 +1,13 @@
 /*
-For this section of the skills check you will be putting together a cheat sheet for common git commands.  You'll provide the command as well as what it does. 
+    For this section of the skills check you will be putting together a cheat sheet for common git commands.  
+    You'll provide the command as well as what it does. 
 */
 
 //////////////////PROBLEM 1////////////////////
-
-//Create a variable called 'gitDefinition'.  It should be a string containing your best definition of what Git is
+/*
+    Create a variable called 'gitDefinition'.
+    It should be a string containing your best definition of what Git is
+*/
 
 // CODE HERE
 
@@ -14,7 +17,7 @@ For this section of the skills check you will be putting together a cheat sheet 
 
 // CODE HERE
 
-//////////////////PROBLEM 3 - 8////////////////////
+//////////////////PROBLEM 3 - 9////////////////////
 
 //For the next several problems you will be creating objects containing information about different git commands.  Each object should contain 'description' and 'code' properties.  The 'description' property will be a string with a description of what that git command does.  The 'code' property should be a string of the actual command (What you type into your terminal).
 
